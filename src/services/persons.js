f8be90f0ad1170b57e267a67a207e273d09d6904
Evan Liu <140853704+evanliu-at-icstudents@users.noreply.github.com>
2024-12-18 08:27:16 -0600

    bruh

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/api/persons';
+const baseUrl = 'https://phonebook-qwec.onrender.com/api/persons';
 
 const getAll = () => {
   return axios.get(baseUrl).then(response => response.data);
